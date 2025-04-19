2959231ca3e84b3075eca90b07c94a82fe7c47bb
pohong <qzawxs24489476@gmail.com>
2025-04-19 17:20:14 +0900

    更新說明

--- Assets/Plugins/Again/Runtime/Components/Managers/AudioManager.cs
+++ Assets/Plugins/Again/Runtime/Components/Managers/AudioManager.cs
@@ -9,6 +9,12 @@ namespace Again.Runtime.Components.Managers
         public void PlaySound(string soundName)
         {
             var audioClip = Resources.Load<AudioClip>($"Sounds/{soundName}");
+            if (audioClip == null)
+            {
+                Debug.Log($"找不到音效：{soundName}");
+                return;
+            }
+
             audioSource.PlayOneShot(audioClip);
         }
     }
